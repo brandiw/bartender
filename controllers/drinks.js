b64f7704e97649ad4168e05768efd149e6d48682
brandiw <brandimichelle.0911@gmail.com>
2019-04-17 11:38:57 -0700

    Stubbing out drinks routes and views

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -7,9 +7,24 @@ let router = express.Router()
 // Reference the models
 // let db = require('../models')
 
-// GET /profile
+// GET /drinks
 router.get('/', (req, res) => {
-  res.send('PROFILE STUB PAGE')
+  res.render('drinks/index')
+})
+
+// POST /drinks
+router.post('/', (req, res) => {
+  res.send(req.body)
+})
+
+// GET /drinks/new
+router.get('/new', (req, res) => {
+  res.render('drinks/new')
+})
+
+// GET /drinks/:id
+router.get('/:id', (req, res) => {
+  res.render('drinks/show')
 })
 
 // Export the routes from this file
