5032a872aa183eb211c3383619afdde9676d8690
brandiw <brandimichelle.0911@gmail.com>
2020-01-09 14:11:57 -0800

    Added ingredients new form

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -21,5 +21,20 @@ router.get('/', (req, res) => {
   })
 })
 
+router.post('/', (req, res) => {
+  db.ingredient.create(req.body)
+  .then(() => {
+    res.redirect('/ingredients')
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('404')
+  })
+})
+
+router.get('/new', (req, res) => {
+  res.render('ingredients/new')
+})
+
 // Export the routes from this file
 module.exports = router
